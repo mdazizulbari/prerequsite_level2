0135ec5a88eea87609104891a798af38d4097eb1
smabari <mdazizulbarishafin@gmail.com>
2025-09-04 03:27:31 +0600

    full definition

--- 1.test.js
+++ 1.test.js
@@ -3,4 +3,12 @@
 //  programming language,
 //  with first-class functions.
 
-// While it is 
+// While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache, CouchDB and Adobe Acrobat.
+
+// JS is a prototype-based,
+//  multi-paradigm,
+//  single-threaded,
+//  dynamic language,
+//  supporting object-oriented, 
+//  imperative, 
+//  non declarative (e.g. functional programming) styles
